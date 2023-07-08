0f2fe9d189c36c60cc5375fa6803426c073a7583
KojiYoshioka <koji.yoshioka@rapyuta-robotics.com>
2023-07-09 08:58:34 +0900

    Initial Commit

---
+++ TryEssentia/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  TryEssentia
+//
+//  Created by koji on 2023/07/09.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ TryEssentia/AppDelegate.m
@@ -0,0 +1,32 @@
+//
+//  AppDelegate.m
+//  TryEssentia
+//
+//  Created by koji on 2023/07/09.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (strong) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+
+- (BOOL)applicationSupportsSecureRestorableState:(NSApplication *)app {
+    return YES;
+}
+
+
+@end
