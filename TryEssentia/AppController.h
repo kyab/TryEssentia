201c00628da5c9ce364375cb8df3d0872386ccca
KojiYoshioka <koji.yoshioka@rapyuta-robotics.com>
2023-07-11 09:38:57 +0900

    initial

---
+++ TryEssentia/AppController.h
@@ -0,0 +1,16 @@
+//
+//  AppController.h
+//  TryEssentia
+//
+//  Created by koji on 2023/07/09.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface AppController : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TryEssentia/AppController.mm
@@ -0,0 +1,18 @@
+//
+//  AppController.m
+//  TryEssentia
+//
+//  Created by koji on 2023/07/09.
+//
+
+#import "AppController.h"
+
+@implementation AppController
+-(void)awakeFromNib{
+    NSLog(@"awakeFromNib");
+}
+- (IBAction)btnClicked:(id)sender {
+    NSLog(@"btnClicked");
+}
+
+@end
