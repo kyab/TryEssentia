0214a516c6ab9407aead8656a22fb2b4eb88fd9a
KojiYoshioka <koji.yoshioka@rapyuta-robotics.com>
2023-07-11 09:43:40 +0900

    commit some stuff

--- TryEssentia/AppController.mm
+++ TryEssentia/AppController.mm
@@ -7,12 +7,19 @@
 
 #import "AppController.h"
 
+#include <essentia/algorithmfactory.h>
+
+using namespace std;
+using namespace essentia;
+
 @implementation AppController
 -(void)awakeFromNib{
     NSLog(@"awakeFromNib");
 }
 - (IBAction)btnClicked:(id)sender {
     NSLog(@"btnClicked");
+    essentia::init();
+    NSLog(@"essentia initialized");
 }
 
 @end
