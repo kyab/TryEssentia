d95f3716cc996a06e612a8323a1ba91a02cde95a
KojiYoshioka <koji.yoshioka@rapyuta-robotics.com>
2023-07-12 09:26:47 +0900

    etc

--- TryEssentia/AppController.mm
+++ TryEssentia/AppController.mm
@@ -8,9 +8,11 @@
 #import "AppController.h"
 
 #include <essentia/algorithmfactory.h>
+#include <essentia/pool.h>
 
 using namespace std;
 using namespace essentia;
+using namespace standard;
 
 @implementation AppController
 -(void)awakeFromNib{
@@ -20,6 +22,38 @@ using namespace essentia;
     NSLog(@"btnClicked");
     essentia::init();
     NSLog(@"essentia initialized");
+    
+    Real sampleRate = 44100.0;
+    AlgorithmFactory &factory = AlgorithmFactory::instance();
+    
+    Algorithm *audioLoader = factory.create("MonoLoader",
+                                           "filename", "/Users/koji/Desktop/hibana.wav",
+                                           "sampleRate", sampleRate);
+    
+    Algorithm *beatTracker = factory.create("BeatTrackerMultiFeature");
+    
+    vector<Real> audio;
+    vector<Real> beats;
+    Real confidence;
+   
+    audioLoader->output("audio").set(audio);
+    audioLoader->compute();
+    
+    beatTracker->input("signal").set(audio);
+    beatTracker->output("ticks").set(beats);
+    beatTracker->output("confidence").set(confidence);
+    beatTracker->compute();
+    
+    NSLog(@"audio size = %lu",audio.size());
+    NSLog(@"beats size = %lu",beats.size());
+    
+    for ( Real b : beats){
+        NSLog(@"beat = %f[sec]",b);
+    }
+    
+    delete audioLoader;
+    delete beatTracker;
+    essentia::shutdown();
 }
 
 @end
